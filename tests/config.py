af09430a0cfba3d91a93d1e7a3ea4bcd2a7cc912
Utugoed <petr.fanernyi@gmail.com>
2022-07-11 13:40:10 +0200

    Tests were fixed

--- tests/config.py
+++ tests/config.py
@@ -3,10 +3,10 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     
-    TG_TOKEN: str
+    TG_TOKEN: str = ""
 
-    VK_TOKEN: str
-    VK_API_VERSION: str
+    VK_TOKEN: str = ""
+    VK_API_VERSION: str = ""
 
     class Config:
         case_sensitive = True
