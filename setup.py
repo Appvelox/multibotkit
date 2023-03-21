b4f96ef538f4df53f1d1ab6775f2d651669335da
Vladislav Volkov <volkov@appvelox.ru>
2023-03-21 17:03:52 +0300

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multibotkit",
-    version="0.1.7",
+    version="0.1.8",
     author="Appvelox LLC",
     author_email="team@appvelox.ru",
     description="Functional library for developing multiplatform chatbots",
